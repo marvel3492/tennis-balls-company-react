341018ca54f35e0ad386bd37fe6e0542b48e6f95
marvel3492 <mariovelasco226@gmail.com>
2026-01-07 12:12:35 -0600

    Add frontend home page and update routing
    Introduced a new Index.js component as the home page in the frontend and updated App.js to route '/' to this component. Commented out backend route usage for the index route, shifting home page rendering responsibility to the frontend.

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,7 @@ var logger = require('morgan');
 const cors = require('cors');
 
 // Routes
-var indexRouter = require('./routes/index');
-var aboutRouter = require('./routes/about');
+// var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -24,8 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 // Use routes
-app.use('/', indexRouter);
-app.use('/about', aboutRouter);
+// app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./views/About";
 import Contact from "./views/Contact";
+import Index from "./views/Index";
 
 /*
 function App() {
@@ -32,6 +33,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Index />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
